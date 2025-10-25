e5b4517c35b4de83980f9a71a384cf1aaafacc88
Shaikh Mahek <mahekshaikh1298@gmail.com>
2025-10-25 22:44:01 +0530

    Enhance square interaction with touch events
    Added touch event listeners for mobile support.

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ for (let i = 0; i < SQUARES; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  square.addEventListener("mouseover", () => setColor(square));
-  square.addEventListener("mouseout", () => removeColor(square));
+ square.addEventListener("mouseover", () => setColor(square));
+square.addEventListener("mouseout", () => removeColor(square));
+square.addEventListener("touchstart", () => setColor(square));
+square.addEventListener("touchend", () => removeColor(square));
+
 
   container.appendChild(square);
 }
